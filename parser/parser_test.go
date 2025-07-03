02fb2a0df25826af754bdbb669b37e80baea22b3
isochikuwa <isochikuwa0522@gmail.com>
2025-07-04 06:54:33 +0900

    fix parser tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -6,11 +6,24 @@ import (
 	"github.com/isochikuwa/monkey/lexer"
 )
 
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
+}
+
 func TestLetStatement(t *testing.T) {
 	input := `
 	let x = 5;
 	let y = 10;
-	let 838383;
+	let foobar = 838383;
 	`
 
 	l := lexer.New(input)
@@ -64,17 +77,4 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 
 	return true
-}
-
-func checkParserErrors(t *testing.T, p *Parser) {
-	errors := p.Errors()
-	if len(errors) == 0 {
-		return
-	}
-
-	t.Errorf("parser has %d errors", len(errors))
-	for _, msg := range errors {
-		t.Errorf("parser error: %q", msg)
-	}
-	t.FailNow()
 }
