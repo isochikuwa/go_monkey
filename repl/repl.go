0485d4027550704c87cb1630d54999c0e57e2c11
isochikuwa <isochikuwa0522@gmail.com>
2025-07-14 07:09:26 +0900

    modify repl

--- repl/repl.go
+++ repl/repl.go
@@ -4,8 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+
 	"github.com/isochikuwa/monkey/lexer"
-	"github.com/isochikuwa/monkey/token"
+	"github.com/isochikuwa/monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -22,9 +23,20 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
