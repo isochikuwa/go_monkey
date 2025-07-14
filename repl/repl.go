f5dd7674001f7d7bca157ccfce2f922abaea8814
isochikuwa <isochikuwa0522@gmail.com>
2025-07-15 07:04:23 +0900

    add let statements evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/isochikuwa/monkey/evaluator"
 	"github.com/isochikuwa/monkey/lexer"
+	"github.com/isochikuwa/monkey/object"
 	"github.com/isochikuwa/monkey/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironemnt()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -31,7 +33,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
